12e4fbee0d0279d2db5bdc5e40f4159f7e47cd7c
valera <valerij.bancer@gmail.com>
2013-04-22 14:04:14 +0200

    Unit test update

--- MARV-TEST/src/com/marv/ui/components/tests/HomePageTest.java
+++ MARV-TEST/src/com/marv/ui/components/tests/HomePageTest.java
@@ -1,5 +1,11 @@
 package com.marv.ui.components.tests;
 
+import static org.junit.Assert.*;
+
+import java.util.regex.Matcher;
+
+import net.sourceforge.jwebunit.api.IElement;
+
 import org.junit.*;
 import static net.sourceforge.jwebunit.junit.JWebUnit.*;
 
@@ -35,8 +41,18 @@ public class HomePageTest {
 	public void testSignInLink() {
 		beginAt("/?command=Home");
 		assertLinkPresentWithExactText("Sign-In");
-		clickLinkWithExactText("Sign-In");
 		assertElementPresent("janrainModal");
-        assertTitleEquals("MARV - home");
+
+        IElement signInBox = getElementById("janrainModal");
+        String signInBoxStyle = signInBox.getAttribute("style");
+        // assert that sign in box is hidden
+        assertTrue(signInBoxStyle.contains("display: none;"));
+        
+		clickLinkWithExactText("Sign-In");
+		
+        signInBox = getElementById("janrainModal");
+        signInBoxStyle = signInBox.getAttribute("style");
+        // assert that sign in box is visible
+        assertTrue(signInBoxStyle.contains("display: block;"));
 	}
 }
